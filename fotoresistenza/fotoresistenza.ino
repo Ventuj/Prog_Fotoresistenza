8e4cf2458edd65f2d1e70b9d6829faa48252ab41
Daniele <Venturin>
2019-11-29 12:33:44 +0100

    Bugfix

--- fotoresistenza/fotoresistenza.ino
+++ fotoresistenza/fotoresistenza.ino
@@ -1,15 +1,15 @@
 //Pin connected to SH_CP of 74HC595
 int clockPin = 8; //pin 11
+
 //Pin connected to ST_CP of 74HC595
 int latchPin = 9; //pin 12
-////Pin connected to DS of 74HC595
-int dataPin = 10; //pin 14
-int tempo = 50;
 
+//Pin connected to DS of 74HC595
+int dataPin = 10; //pin 14
+
+int tempo = 50;
 int led = 7;
 int res = A0;
-
-int luminosita;
 
 void setup(){
     pinMode(dataPin, OUTPUT);
@@ -20,7 +20,8 @@ void setup(){
     Serial.begin(9600);
 }
 
-byte mover[] = {
+byte var[] = {
+  B00000000,
   B11111111,
   B01111111,
   B00111111,
@@ -29,40 +30,42 @@ byte mover[] = {
   B00000111,
   B00000011,
   B00000001,
-  B00000000,
 };
+
 int i = 8;
+
 void loop(){
+  
   Serial.println(String(analogRead(res)));
-  luminosita = analogRead(res);  //Lettura della luminosità
-  luminosita = luminosita/4;  //Adeguiamo il valore della luminosità
-  analogWrite(led,luminosita);
+
   if(analogRead(res) > 100 && analogRead(res) < 200){
-    i = 0;
-  }
-  if(analogRead(res) > 200 && analogRead(res) < 300){
     i = 1;
   }
-  if(analogRead(res) > 300 && analogRead(res) < 400){
+  if(analogRead(res) > 200 && analogRead(res) < 300){
     i = 2;
   }
-  if(analogRead(res) > 400 && analogRead(res) < 500){
+  if(analogRead(res) > 300 && analogRead(res) < 400){
     i = 3;
   }
-  if(analogRead(res) > 500 && analogRead(res) < 600){
+  if(analogRead(res) > 400 && analogRead(res) < 500){
     i = 4;
   }
-  if(analogRead(res) > 600 && analogRead(res) < 700){
+  if(analogRead(res) > 500 && analogRead(res) < 600){
     i = 5;
   }
-  if(analogRead(res) > 700 && analogRead(res) < 800){
+  if(analogRead(res) > 600 && analogRead(res) < 700){
     i = 6;
   }
-  if(analogRead(res) > 800){
+  if(analogRead(res) > 700 && analogRead(res) < 800){
     i = 7;
   }
+  if(analogRead(res) > 800){
+    i = 0;
+  }
+
   digitalWrite(latchPin, LOW);
-  shiftOut(dataPin, clockPin, MSBFIRST, mover[i]);
+  shiftOut(dataPin, clockPin, MSBFIRST, var[i]);
   digitalWrite(latchPin, HIGH);
   delay(5);
+
 }
